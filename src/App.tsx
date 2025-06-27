e91972d2e1f99426c43fc9e1046a01b26db4d7d8
Syed-Hermain <mohd.hermain2004@gmail.com>
2025-06-27 20:13:07 +0530

    fixed theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Button } from "@/components/ui/button"
-
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "./components/mode-toggle"
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>
+        <ModeToggle/>
+        <h1 className="text-3xl font-bold underline text-center text-white bg-background">
+          Hello Vite + React!
+        </h1>
+        <p className="text-center text-gray-500">
+          This is a simple example of a Vite + React application with Tailwind CSS.
+        </p>
+      </div>
+    </ThemeProvider>
   )
 }
 
